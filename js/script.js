ace934dc243ef1cd224a750209e89bb1d8aa6f14
Bohdan Izbash <107304090+BohdanIzbash@users.noreply.github.com>
2022-06-13 18:24:27 +0300

    js practic files added

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+"use strict"
+console.log('Я вчу JavaScript')
+
+
+
+let myFirst = 'obizbash';
+console.log(myFirst);
+
+
+let ayeColor = 'blue';
+
+console.log(ayeColor);
+
+
+
+let user = '';
+let userName = 'Вася';
+user = userName;
+console.log(user);
+
+
+
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(answer);
+
+
+//const answers = [];
+
+
+
+const category = 'toys';
+
+console.log(`http:someurl.com/${category}/5`);
